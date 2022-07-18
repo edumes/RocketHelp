339731e4beae5a5f7bd4f25d8cb0fa5fd2711cda
edumes <66120423+edumes@users.noreply.github.com>
2022-07-18 12:19:43 -0300

    Aula 1 & 2

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { THEME } from './src/styles/theme';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import {SignIn} from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+      {fontsLoaded ? <SignIn/> : <Loading/>}
+    </NativeBaseProvider>
+  );
+}
