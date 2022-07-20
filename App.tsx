774f97627978efa06aa5299272c9fdb59cb97ed5
edumes <66120423+edumes@users.noreply.github.com>
2022-07-19 21:09:42 -0300

    Aula 3

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import {SignIn} from './src/screens/SignIn';
+import {Home} from './src/screens/Home';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {fontsLoaded ? <SignIn/> : <Loading/>}
+      {fontsLoaded ? <Home /> : <Loading/>}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/Home.tsx
@@ -0,0 +1,59 @@
+import { HStack, IconButton, VStack, useTheme, Text, Heading } from 'native-base';
+import { useState } from 'react';
+import { SignOut } from 'phosphor-react-native';
+import Logo from '../assets/logo_secondary.svg';
+import { Filter } from '../components/Filter';
+
+export function Home() {
+  const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
+  const { colors } = useTheme();
+
+  return (
+    <VStack flex={1} pb={6} bg="gray.700">
+      <HStack
+      w="full"
+      justifyContent="space-between"
+      alignItems="center"
+      bg="gray.600"
+      pt={12}
+      pb={5}
+      px={6}
+      >
+        <Logo />
+        <IconButton
+        icon={ <SignOut size={26} color={colors.gray[300]}/> }
+        />
+
+      </HStack>
+
+      <VStack flex={1} px={6}>
+        <HStack w="full" mt={8} mb={4} justifyContent="space-between" alignContent="center">
+          <Heading color="gray.100">
+            Meus chamados
+          </Heading>
+
+        <Text color="gray.200">
+          1
+        </Text>
+        </HStack>
+
+        <HStack space={3} mb={8}>
+        <Filter
+          type='open'
+          title='em andamento'
+          onPress={() => setStatusSelected('open')}
+          isActive={statusSelected === 'open'}
+        />
+
+        <Filter
+          type='closed'
+          title='finalizados'
+          onPress={() => setStatusSelected('closed')}
+          isActive={statusSelected === 'closed'}
+        />
+        </HStack>
+
+      </VStack>
+    </VStack>
+  );
+}
