f877adcbf042cd56edf1719ba1e0593c2fdd43b6
edumes <66120423+edumes@users.noreply.github.com>
2022-08-04 14:58:07 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import {Home} from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {fontsLoaded ? <Home /> : <Loading/>}
+      {fontsLoaded ? <Routes /> : <Loading/>}
     </NativeBaseProvider>
   );
 }
